1e5d8cabcf669cdf9e55599564f4de04c19eca98
Richard Trotta <trotta@cua.edu>
2023-01-25 02:25:03 -0500

    Added bookmark grabbing and uploading to evernote. Working on summarizing articles to add to notes.

--- src/GetBookmarks.py
+++ src/GetBookmarks.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-01-25 02:24:23 trottar"
+# Time-stamp: "2023-01-25 02:25:01 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -33,7 +33,7 @@ def import_bookmarks():
     for folder in chrome_bookmarks.folders:
         if "Must Read" == folder.name:
             print("\nImporting data for bookmarks from {}...".format(folder.name))
-            for i,url in tqdm(enumerate(folder.urls), position=0, bar_format="{bar}"):
+            for i,url in tqdm(enumerate(folder.urls), bar_format="{bar}"):
                 bookmarkDict.update({"folder" : folder.name})
                 bookmarkDict.update({"title" : url.name.lower()})
                 bookmarkDict.update({"url" : url.url})
