f1c52ab196882f7b8a9ae90df3c290faa9e73f85
Richard Trotta <trotta@cua.edu>
2023-01-25 15:22:36 -0500

    Added bookmark grabbing and uploading to evernote. Working on summarizing articles to add to notes.

--- src/CreateEvernote.py
+++ src/CreateEvernote.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-01-25 15:21:15 trottar"
+# Time-stamp: "2023-01-25 15:22:33 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -52,7 +52,7 @@ def makeNote(authToken, noteStore, noteTitle, noteBody, noteTag=None, parentNote
     ## Attempt to create note in Evernote account
     try:
         if noteTag != None:
-            ourTag = noteStore.getTag(noteTag)
+            ourTag = noteStore.getTag(noteTag.guid)
             ourNote.tagGuids = [ourTag.guid]
         note = noteStore.createNote(authToken, ourNote)
     except Errors.EDAMUserException as edue:
