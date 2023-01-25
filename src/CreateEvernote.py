4825d9cf15c4b9472d329cf8c73696cb9e83a19e
Richard Trotta <trotta@cua.edu>
2023-01-25 16:27:48 -0500

    Added bookmark grabbing and uploading to evernote. Working on summarizing articles to add to notes.

--- src/CreateEvernote.py
+++ src/CreateEvernote.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-01-25 16:25:42 trottar"
+# Time-stamp: "2023-01-25 16:27:46 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -25,14 +25,7 @@ def makeTag(noteStore, nameTag):
         print("!!!",t.name)
         tags_list.append(t.name)
     if ourTag.name in tags_list:
-        for t in tags:
-            if ourTag.name == t:
-                ourTag = t
-        #ourTag = noteStore.getTag(ourTag.name)
-        print("b",ourTag.guid)
-        return ourTag
-    
-    else:
+
         print("\nAdding tag {}...\n".format(ourTag.name))
         ourTag = noteStore.createTag(ourTag)
         print("a",ourTag.guid)
