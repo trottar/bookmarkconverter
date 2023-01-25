d7018d6bf20a19f443082849b3c2ea5cae4e6532
Richard Trotta <trotta@cua.edu>
2023-01-25 18:27:52 -0500

    Added bookmark grabbing and uploading to evernote. Working on summarizing articles to add to notes.

--- src/CreateEvernote.py
+++ src/CreateEvernote.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-01-25 18:22:58 trottar"
+# Time-stamp: "2023-01-25 18:27:48 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -50,7 +50,7 @@ def makeNote(noteStore, noteTitle, noteBody, noteTag=None, parentNotebook=None):
         print(note_filter)
         note_filter.words = ourNote.title
         print(note_filter.words)
-        search_result = noteStore.findNotes(note_filter, 0, 1)
+        search_result = noteStore.findNoteCounts(note_filter)
         print(search_result)
  
         if search_result.totalNotes > 0:
