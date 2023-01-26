bb5a692bb113b5d5ae2af6be676b1b5f8e728f21
Richard Trotta <trotta@cua.edu>
2023-01-25 23:32:33 -0500

    Script is fully working! Testing for all bookmarks!

--- src/GetBookmarks.py
+++ src/GetBookmarks.py
@@ -3,7 +3,7 @@
 #
 # Description:
 # ================================================================
-# Time-stamp: "2023-01-25 23:23:43 trottar"
+# Time-stamp: "2023-01-25 23:32:04 trottar"
 # ================================================================
 #
 # Author:  Richard L. Trotta III <trotta@cua.edu>
@@ -33,36 +33,36 @@ def import_bookmarks(noteStore, notebooks):
 
     for folder in chrome_bookmarks.folders:
         #if "Workout" == folder.name:
-        if "Workout" == folder.name or "Must Read" == folder.name:
-            print("\nImporting data for bookmarks from {}...".format(folder.name))
-            for i,url in enumerate(folder.urls):
-                nb_name_list = []
-                nb_list = []
-                for nb in notebooks:
-                    NoteExists = False
-                    if nb.name == folder.name:
-                        note_filter = NoteFilter(notebookGuid=nb.guid)
-                        search_result = noteStore.findNotes(note_filter, 0, 100000)
-                        for note in search_result.notes:
-                            if url.name.lower() == note.title:
-                                print("{} -> Note already exists".format(url.name.lower()))
-                                NoteExists = True
-                                break
-                    if NoteExists == True:
-                        break
-                if NoteExists == False:
-                    bookmarkDict.update({"folder" : folder.name})
-                    bookmarkDict.update({"title" : url.name.lower()})
-                    bookmarkDict.update({"url" : html.escape(url.url)})
-                    bookmarkDict.update({"summary" : grabText(html.escape(url.url))})
-                    #print("\t-> ",url.name.lower())
-                    bookmarkDict = {k : bookmarkDict[k] for k in sorted(bookmarkDict.keys())}
-                    df = df.append(bookmarkDict,ignore_index=True)
-                    if len(folder.urls) > 1:
-                        Tools.progressBar(i, len(folder.urls)-1,bar_length=25)
-                    else:
-                        Tools.progressBar(i, len(folder.urls),bar_length=25)
-            print("\n")
+        #if "Workout" == folder.name or "Must Read" == folder.name:
+        print("\nImporting data for bookmarks from {}...".format(folder.name))
+        for i,url in enumerate(folder.urls):
+            nb_name_list = []
+            nb_list = []
+            for nb in notebooks:
+                NoteExists = False
+                if nb.name == folder.name:
+                    note_filter = NoteFilter(notebookGuid=nb.guid)
+                    search_result = noteStore.findNotes(note_filter, 0, 100000)
+                    for note in search_result.notes:
+                        if url.name.lower() == note.title:
+                            print("{} -> Note already exists".format(url.name.lower()))
+                            NoteExists = True
+                            break
+                if NoteExists == True:
+                    break
+            if NoteExists == False:
+                bookmarkDict.update({"folder" : folder.name})
+                bookmarkDict.update({"title" : url.name.lower()})
+                bookmarkDict.update({"url" : html.escape(url.url)})
+                bookmarkDict.update({"summary" : grabText(html.escape(url.url))})
+                #print("\t-> ",url.name.lower())
+                bookmarkDict = {k : bookmarkDict[k] for k in sorted(bookmarkDict.keys())}
+                df = df.append(bookmarkDict,ignore_index=True)
+                if len(folder.urls) > 1:
+                    Tools.progressBar(i, len(folder.urls)-1,bar_length=25)
+                else:
+                    Tools.progressBar(i, len(folder.urls),bar_length=25)
+        print("\n")
     print("-"*70)
     print(df)
     print("-"*70)
